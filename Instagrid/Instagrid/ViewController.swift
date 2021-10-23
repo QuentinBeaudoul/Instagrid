bc68e1b5cb93d84570de8b9a4a085cb9f89a177b
Quentin Beaudoul <qbeaudoul@icloud.com>
2021-10-23 18:07:21 +0200

    gestion du clique et du changement de view

--- Instagrid/Instagrid/ViewController.swift
+++ Instagrid/Instagrid/ViewController.swift
@@ -9,9 +9,59 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    
+    @IBOutlet var selectedViews: [UIImageView]!
+    @IBOutlet var compositionViews: [UIView]!
+    @IBOutlet var viewLayouts: [UIView]!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        gesturesEvents()
+    }
+    
+    private func gesturesEvents(){
+        for (_, viewLayout) in viewLayouts.enumerated() {
+            let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleTap(_:)))
+            viewLayout.addGestureRecognizer(tapGesture)
+        }
+    }
+    
+    @objc private func handleTap(_ sender: UITapGestureRecognizer? = nil){
+        
+        guard let tag = sender?.view?.tag else { print("unroconized"); return }
+        
+        hideAllSelected()
+        hideAllCompositionViews()
+        selectedViews[tag].isHidden = false
+        compositionViews[tag].isHidden = false
+        /*switch tag {
+        case 0 :
+            hideAllSelected()
+            selectedViews[tag].isHidden = false
+            print("view 1 selected")
+        case 1 :
+            selectedViews[tag].isHidden = false
+            print("view 2 selected")
+        case 2 :
+            print("view 3 selected")
+        default:
+            print("default")
+        }*/
+    }
+    
+    private func hideAllSelected(){
+        for selectedView in selectedViews {
+            selectedView.isHidden = true
+        }
+    }
+    private func hideAllCompositionViews(){
+        for compositionView in compositionViews {
+            compositionView.isHidden = true
+        }
     }
 
 
